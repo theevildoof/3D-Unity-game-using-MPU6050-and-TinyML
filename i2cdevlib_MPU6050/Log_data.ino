314bef794024bec38bc11a5f2a56b31492cab243
Vishwa <vishwa181@gmail.com>
2022-06-24 13:02:29 +0530

    Update Log_data.ino

--- i2cdevlib_MPU6050/Log_data.ino
+++ i2cdevlib_MPU6050/Log_data.ino
@@ -1,1 +1,124 @@
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "Wire.h"
 
+
+MPU6050 mpu(0x69);
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+int count =1;
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+
+  Serial.begin(115200);
+
+  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+  mpu.initialize();
+  devStatus = mpu.dmpInitialize();
+  mpu.setXGyroOffset(21); //++
+  mpu.setYGyroOffset(-22); //--
+  mpu.setZGyroOffset(28);
+
+  if (devStatus == 0) {
+    mpu.setDMPEnabled(true);
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    dmpReady = true;
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // Error
+    Serial.println("Error!");
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (!dmpReady) {
+      Serial.println("IMU not connected.");
+      delay(10);
+      return;
+    }
+
+    int  mpuIntStatus = mpu.getIntStatus();
+    fifoCount = mpu.getFIFOCount();
+
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) { // check if overflow
+      mpu.resetFIFO();
+    } else if (mpuIntStatus & 0x02) {
+      while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+      fifoCount -= packetSize;
+      detect_motion();
+    }
+}
+
+void detect_motion(){
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetAccel(&aa, fifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+  mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+  
+  if( abs(aaWorld.x) +abs(aaWorld.y) + abs(aaWorld.y) > 15000){
+    read_data();
+  }
+  else{
+    delay(5);
+  }
+}
+
+void read_data(){
+   for(int i =0;i<720;i++){
+
+    int  mpuIntStatus = mpu.getIntStatus();
+    fifoCount = mpu.getFIFOCount();
+
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) { // check if overflow
+      mpu.resetFIFO();
+    } else if (mpuIntStatus & 0x02) {
+      while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+      fifoCount -= packetSize;
+     SendWorldAccel();  
+    }
+    }
+    Serial.println("");
+  Serial.println("--------");
+  Serial.println(count);
+  Serial.println("--------");
+  count++;
+  }
+
+void SendWorldAccel() {
+  // display initial world-frame acceleration, adjusted to remove gravity
+  // and rotated based on known orientation from quaternion
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetAccel(&aa, fifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+  mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+  Serial.print(aaWorld.x );
+  Serial.print(",");
+  Serial.print(aaWorld.y);
+  Serial.print(",");
+  Serial.println(aaWorld.z);
+}
